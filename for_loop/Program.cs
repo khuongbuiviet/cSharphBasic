a6bf66c7917d3960cb5a39363cdd432af30ff239
Khuong Bui <buikhuong@my.smccd.edu>
2020-08-26 07:14:11 +0700

    upload 260820200713AM

--- for_loop/Program.cs
+++ for_loop/Program.cs
@@ -10,7 +10,15 @@ namespace for_loop
     {
         static void Main(string[] args)
         {
-
-        }
+            for(int i = 0; i < 100; i++)
+            {
+                Console.Write("{0} ", i);
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine("\t{0}", i);
+                } // end if
+            } //end for
+            Console.ReadLine();
+        } // end main
     }
 }
---
+++ nested_for/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nested_for
+{
+    class Program
+    {
+        private void ComputeSums()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            
+        } // end main
+    }
+}
